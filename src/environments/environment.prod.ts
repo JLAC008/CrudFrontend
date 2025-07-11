446cd14c23534d35743ccf9d1e4d29f2a5ae2412
alejandrofm98 <alejandrofmayor98@gmail.com>
2025-07-12 00:15:23 +0200

    Update development API URL and comment out production environment configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
-export const environment = {
-  production: true,
-  apiUrl: 'http://jlacdev.es:8090/api/empleados'
-};
+// export const environment = {
+//   production: true,
+//   apiUrl: 'http://jlacdev.es:8090/api/empleados'
+// };
