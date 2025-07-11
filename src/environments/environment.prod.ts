7816facf4b44b736c038f0544b8f7c7f7d600a0c
alejandrofm98 <alejandrofmayor98@gmail.com>
2025-07-11 23:26:36 +0200

    Add Docker setup with Nginx, update environment API URLs, and configure docker-compose integration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://produccionapi:8080/api/empleados'
+  apiUrl: 'http://produccionapi:8090/api/empleados'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api/empleados'
+  apiUrl: 'http://localhost:8090/api/empleados'
 };
